3cb93f2e78a49eedfdd2bf9d3d0f31babc2f0bfd
Kanno Beck <drkannobeck@gmail.com>
2023-07-14 13:38:02 +0100

    update

--- components/CountdownTimer.tsx
+++ components/CountdownTimer.tsx
@@ -55,7 +55,7 @@ const CountdownTimer = () => {
           <div className="uppercase flex">
             <div className="flex flex-col items-center p-2.5 lg:p-4 border-t mr-4 lg:mr-6 rounded-md w-[94px]">
               <span className="text-lg lg:text-[40px] sm:text-2xl font-black">{days}</span>
-              <span className="text-[14.7px] sm:text-[15px] lg:text-lg text-gray-400 font-medium pt-4 md:pt-6">Days</span>
+              <span className="text-[14.7px] text-gray-400 font-medium pt-4 md:pt-6">Days</span>
             </div>
 
             <div className="flex flex-col items-center p-2.5 lg:p-4 border-t mr-4 lg:mr-6 rounded-md w-[94px]">
